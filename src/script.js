438e5fc2edd481a9fdda2697f7d5712787b146ba
Theus <matheusdesouzaoliveira32@gmail.com>
2024-06-01 23:08:07 -0300

    Atualização do JS e mudança de links da lista de games

--- src/script.js
+++ src/script.js
@@ -2,6 +2,7 @@ let logged = sessionStorage.getItem("logged");
 const session = localStorage.getItem("session");
 const btnSair = document.getElementById("sair")
 
+
 function checkLogged() {
     if(session) {
         sessionStorage.setItem("logged", session);
@@ -39,63 +40,61 @@ function getAccount(key) {
 function logout() {
     sessionStorage.removeItem("logged");
     localStorage.removeItem("session");
-
+    
     window.location.href = "tela-inicial.html";
 }
-
 checkLogged();
-
+                
 btnSair.addEventListener("click", logout);
-
-//Criar Conta
-document.getElementById("create-form").addEventListener("submit", function(e) {
-    e.preventDefault();
-
-    const email = document.getElementById("email-create-input").value;
-    const password = document.getElementById("password-create-input").value;
-    const confirmPassword = document.getElementById("password-confirm").value;
-
-    if(email.length < 5) {
-        alert("Preencha o campo com um e-mail válido.")
-        return;
-    }
-
-    if(confirmPassword !== password) {
-        alert("As senhas não são iguais.")
-        return;
-    }
-
-    saveAccount({
-        login: email,
-        password: password,
-    });
-
-    alert("Conta criada com sucesso.");
-    window.location.href = "tela-inicial.html"
-});
 
 //Logar no Sistema
 document.getElementById("login-form").addEventListener("submit", function(e) {
     e.preventDefault();
-
+    
     const email = document.getElementById("email-input").value;
     const password = document.getElementById("password-input").value;
     const checkSession = document.getElementById("session-check").checked;
-
     const account = getAccount(email);
-
+    
     if(!account) {
         alert("Ops! Verifique o usuário ou a senha.")
         return;
     }
-
+    
     if(account) {
         if(account.password !== password) {
             alert("Ops! Verifique o usuário ou a senha.")
             return;
         }
-
+        
         saveSession(email, checkSession);
         window.location.href = "tela-inicial.html";
     }
 })
+
+//Criar Conta
+document.getElementById("create-form").addEventListener("submit", function(e) {
+        e.preventDefault();
+    
+        const email = document.getElementById("email-create-input").value;
+        const password = document.getElementById("password-create-input").value;
+        const confirmPassword = document.getElementById("password-confirm").value;
+    
+        if(email.length < 5) {
+                alert("Preencha o campo com um e-mail válido.")
+                return;
+            }
+        
+            if(confirmPassword !== password) {
+                    alert("As senhas não são iguais.")
+                    return;
+                }
+            
+                saveAccount({
+                        login: email,
+                        password: password,
+                    });
+                
+                    alert("Conta criada com sucesso.");
+                    window.location.href = "tela-inicial.html"
+                });
